607ecb012b05781631636cd353d49b94d812d990
shohei-yasutake <shohei-yasutake@cookpad.jp>
2015-04-09 15:37:48 +0900

    Add Command#get

--- lib/rystrix/command.rb
+++ lib/rystrix/command.rb
@@ -2,12 +2,20 @@ require 'rystrix/rich_future'
 
 module Rystrix
   class Command
+
+    NotExecuteYetError = Object.new
+
     def initialize(opts = {}, &block)
       @future = RichFuture.new(&block)
     end
 
     def execute
       @future.execute
+    end
+
+    def get
+      raise NotExecuteYetError if not @future.executed?
+      @future.get
     end
   end
 end
--- spec/rystrix/command_spec.rb
+++ spec/rystrix/command_spec.rb
@@ -12,4 +12,39 @@ describe Rystrix::Command do
       expect(Time.now - start).to be < 1
     end
   end
+
+  describe '#get' do
+    context 'with success' do
+      it 'should return success value' do
+        command = Rystrix::Command.new do
+          42
+        end
+        command.execute
+        expect(command.get).to eq(42)
+      end
+    end
+
+    context 'with sleep and success' do
+      it 'should block and return success value' do
+        start = Time.new
+        command = Rystrix::Command.new do
+          sleep 0.1
+          42
+        end
+        command.execute
+        expect(command.get).to eq(42)
+        expect(Time.now - start).to be > 0.1
+      end
+    end
+
+    context 'with failure' do
+      it 'should throw exception' do
+        command = Rystrix::Command.new do
+          raise RuntimeError
+        end
+        command.execute
+        expect { command.get }.to raise_error(RuntimeError)
+      end
+    end
+  end
 end
