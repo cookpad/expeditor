415f1c3346c5b0249cc837a51735276af1f74137
shohei-yasutake <shohei-yasutake@cookpad.jp>
2015-04-09 16:09:09 +0900

    Use command generate function

--- spec/rystrix/command_spec.rb
+++ spec/rystrix/command_spec.rb
@@ -1,21 +1,37 @@
 require 'spec_helper'
 
 describe Rystrix::Command do
+
+  def simple_command(v)
+    Rystrix::Command.new do
+      v
+    end
+  end
+
+  def sleep_command(n, v)
+    Rystrix::Command.new do
+      sleep n
+      v
+    end
+  end
+
+  def error_command(e, v)
+    Rystrix::Command.new do
+      raise e
+      v
+    end
+  end
+
   describe '#execute' do
     it 'should not block' do
       start = Time.now
-      command = Rystrix::Command.new do
-        sleep 1
-        42
-      end
+      command = sleep_command(1, 42)
       command.execute
       expect(Time.now - start).to be < 1
     end
 
     it 'should return self' do
-      command = Rystrix::Command.new do
-        42
-      end
+      command = simple_command(42)
       expect(command.execute).to eq(command)
     end
   end
@@ -23,9 +39,7 @@ describe Rystrix::Command do
   describe '#executed?' do
     context 'with executed' do
       it 'should be true' do
-        command = Rystrix::Command.new do
-          42
-        end
+        command = simple_command(42)
         command.execute
         expect(command.executed?).to be true
       end
@@ -33,9 +47,7 @@ describe Rystrix::Command do
 
     context 'with not executed' do
       it 'should be false' do
-        command = Rystrix::Command.new do
-          42
-        end
+        command = simple_command(42)
         expect(command.executed?).to be false
       end
     end
@@ -44,9 +56,7 @@ describe Rystrix::Command do
   describe '#get' do
     context 'with success' do
       it 'should return success value' do
-        command = Rystrix::Command.new do
-          42
-        end
+        command = simple_command(42)
         command.execute
         expect(command.get).to eq(42)
       end
@@ -55,10 +65,7 @@ describe Rystrix::Command do
     context 'with sleep and success' do
       it 'should block and return success value' do
         start = Time.new
-        command = Rystrix::Command.new do
-          sleep 0.1
-          42
-        end
+        command = sleep_command(0.1, 42)
         command.execute
         expect(command.get).to eq(42)
         expect(Time.now - start).to be > 0.1
@@ -67,9 +74,7 @@ describe Rystrix::Command do
 
     context 'with failure' do
       it 'should throw exception' do
-        command = Rystrix::Command.new do
-          raise RuntimeError
-        end
+        command = error_command(RuntimeError, nil)
         command.execute
         expect { command.get }.to raise_error(RuntimeError)
       end
@@ -77,9 +82,7 @@ describe Rystrix::Command do
 
     context 'with not executed' do
       it 'should throw NotExecutedYetError' do
-        command = Rystrix::Command.new do
-          42
-        end
+        command = simple_command(42)
         expect { command.get }.to raise_error(Rystrix::NotExecutedYetError)
       end
     end
