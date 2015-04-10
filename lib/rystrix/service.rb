5095316e39f668c4bf2021d21de253c522b62156
shohei-yasutake <shohei-yasutake@cookpad.jp>
2015-04-10 12:08:06 +0900

    Add Service#success,failure,rejection,timeout

--- lib/rystrix/service.rb
+++ lib/rystrix/service.rb
@@ -6,6 +6,23 @@ module Rystrix
 
     def initialize(opts = {})
       @executor = Concurrent::ThreadPoolExecutor.new(opts)
+      @bucket = Rystrix::Bucket.new(opts)
+    end
+
+    def success
+      @bucket.increment :success
+    end
+
+    def failure
+      @bucket.increment :failure
+    end
+
+    def rejection
+      @bucket.increment :rejection
+    end
+
+    def timeout
+      @bucket.increment :timeout
     end
   end
 end
