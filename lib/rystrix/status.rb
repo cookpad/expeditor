8eb499ecad589e4135afa5706cc5f7ead2f5a97e
shohei-yasutake <shohei-yasutake@cookpad.jp>
2015-04-13 11:21:05 +0900

    Remove mutex in Status

--- lib/rystrix/status.rb
+++ lib/rystrix/status.rb
@@ -1,23 +1,25 @@
 module Rystrix
   class Status
+    attr_accessor :success
+    attr_accessor :failure
+    attr_accessor :rejection
+    attr_accessor :timeout
+
     def initialize
-      @mutex = Mutex.new
       set(0, 0, 0, 0)
     end
 
     def increment(type)
-      @mutex.synchronize do
-        case type
-        when :success
-          @success += 1
-        when :failure
-          @failure += 1
-        when :rejection
-          @rejection += 1
-        when :timeout
-          @timeout += 1
-        else
-        end
+      case type
+      when :success
+        @success += 1
+      when :failure
+        @failure += 1
+      when :rejection
+        @rejection += 1
+      when :timeout
+        @timeout += 1
+      else
       end
     end
 
@@ -25,55 +27,13 @@ module Rystrix
       set(0, 0, 0, 0)
     end
 
-    def success
-      @mutex.synchronize { @success }
-    end
-
-    def success=(n)
-      @mutex.synchronize do
-        @success = n
-      end
-    end
-
-    def failure
-      @mutex.synchronize { @failure }
-    end
-
-    def failure=(n)
-      @mutex.synchronize do
-        @failure = n
-      end
-    end
-
-    def rejection
-      @mutex.synchronize { @rejection }
-    end
-
-    def rejection=(n)
-      @mutex.synchronize do
-        @rejection = n
-      end
-    end
-
-    def timeout
-      @mutex.synchronize { @timeout }
-    end
-
-    def timeout=(n)
-      @mutex.synchronize do
-        @timeout = n
-      end
-    end
-
     private
 
     def set(s, f, r, t)
-      @mutex.synchronize do
-        @success = s
-        @failure = f
-        @rejection = r
-        @timeout = t
-      end
+      @success = s
+      @failure = f
+      @rejection = r
+      @timeout = t
     end
   end
 end
