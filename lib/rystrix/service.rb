6eccace68fd13594c01ba6bd17f17b525b4b8d5c
shohei-yasutake <shohei-yasutake@cookpad.jp>
2015-04-14 18:59:09 +0900

    Remove a comment

--- lib/rystrix/service.rb
+++ lib/rystrix/service.rb
@@ -40,7 +40,7 @@ module Rystrix
       s = @bucket.total
       total_count = s.success + s.failure + s.timeout
       if total_count > [@non_break_count, 0].max
-        failure_count = s.failure + s.timeout # also rejection?
+        failure_count = s.failure + s.timeout
         failure_count.to_f / total_count.to_f >= @threshold
       else
         false
