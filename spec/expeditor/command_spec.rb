dd254fa01b585b46c9a3c597a1f105a25328c31c
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-08-29 11:01:13 +0900

    Add specs for `start_with_retry` with current thread mode
    Spec

--- spec/expeditor/command_spec.rb
+++ spec/expeditor/command_spec.rb
@@ -79,65 +79,6 @@ describe Expeditor::Command do
         expect(command.get).to eq(1000)
       end
     end
-
-    context 'current thread mode' do
-      context 'with normal' do
-        it 'should execute on current thread' do
-          Thread.current.thread_variable_set('foo', 'bar')
-          command = Expeditor::Command.new do
-            Thread.current.thread_variable_get('foo')
-          end
-          expect(command.start(current_thread: true).get).to eq('bar')
-        end
-
-        it 'should return self' do
-          command = simple_command(42)
-          expect(command.start(current_thread: true)).to eq(command)
-        end
-
-        it 'should ignore from the second time' do
-          count = 0
-          command = Expeditor::Command.new do
-            count += 1
-            count
-          end
-          command.start(current_thread: true)
-          command.start(current_thread: true)
-          command.start(current_thread: true)
-          expect(command.get).to eq(1)
-          expect(count).to eq(1)
-        end
-      end
-
-      context 'with fallback' do
-        it 'should work fallback proc' do
-          command = error_command(error_in_command, nil)
-          command.set_fallback do
-            42
-          end
-
-          expect(command.start(current_thread: true).get).to eq(42)
-        end
-
-        it 'should work fallback on current thread' do
-          Thread.current.thread_variable_set("count", 1)
-          command = Expeditor::Command.new do
-            count = Thread.current.thread_variable_get("count")
-            count += 1
-            Thread.current.thread_variable_set("count", count) # => 2
-            raise error_in_command
-          end
-
-          command.set_fallback do
-            count = Thread.current.thread_variable_get("count")
-            count += 1
-            count # => 3
-          end
-
-          expect(command.start(current_thread: true).get).to eq(3)
-        end
-      end
-    end
   end
 
   describe '#started?' do
