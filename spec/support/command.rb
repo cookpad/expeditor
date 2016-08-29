a30bb2a27bac02106162bd0ab5b6d40d9d7a19fc
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-08-29 10:48:35 +0900

    Change global methods to module methods

--- spec/support/command.rb
+++ spec/support/command.rb
@@ -1,18 +1,24 @@
-def simple_command(v, opts = {})
-  Expeditor::Command.new(opts) do
-    v
+module CommandHelpers
+  def simple_command(v, opts = {})
+    Expeditor::Command.new(opts) do
+      v
+    end
+  end
+
+  def sleep_command(n, v, opts = {})
+    Expeditor::Command.new(opts) do
+      sleep n
+      v
+    end
+  end
+
+  def error_command(e, opts = {})
+    Expeditor::Command.new(opts) do
+      raise e
+    end
   end
 end
 
-def sleep_command(n, v, opts = {})
-  Expeditor::Command.new(opts) do
-    sleep n
-    v
-  end
-end
-
-def error_command(e, opts = {})
-  Expeditor::Command.new(opts) do
-    raise e
-  end
+RSpec.configure do |c|
+  c.include CommandHelpers
 end
