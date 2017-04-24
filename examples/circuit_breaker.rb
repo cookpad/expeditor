d6dd8a150a85ebb886771e91d1f520ec9a68c4f1
Taiki Ono <taiks.4559@gmail.com>
2017-04-24 10:52:50 +0900

    Add example script for circuit breaker

---
+++ examples/circuit_breaker.rb
@@ -0,0 +1,27 @@
+require 'expeditor'
+
+service = Expeditor::Service.new
+
+i = 1
+loop do
+  puts '=' * 100
+  p i
+
+  command = Expeditor::Command.new(service: service, timeout: 1) {
+    sleep 0.001 # simulate remote resource access
+    if File.exist?('foo')
+      'result'
+    else
+      raise 'Demo error'
+    end
+  }.set_fallback { |e|
+    p e
+    'default value'
+  }.start
+
+  p command.get
+  p service.current_status
+  puts
+
+  i += 1
+end
